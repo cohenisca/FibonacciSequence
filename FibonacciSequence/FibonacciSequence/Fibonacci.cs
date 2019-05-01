47d5332261dc5d7b492ef07e0aa8bb722ecc26a9
isca <isccohen@g.jct.ac.il>
2019-05-02 00:11:14 +0300

    finished

---
+++ FibonacciSequence/FibonacciSequence/Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Fibonacci
+    {
+        //recorsive
+        public int fibonacciRec(int count)
+        {
+            if (count < 2)
+                return 1;
+            else
+                return fibonacciRec(count - 1) + fibonacciRec(count - 2);
+        }
+        public void fibonacci_Rec(int num)
+        {
+            for (int i = 0; i < num; i++)
+                Console.Write(fibonacciRec(i).ToString() + " ");
+        }
+
+        //iterative
+        public void fibonacciIt(int count)
+        {
+            int prev1 = 1, prev2 = 1;
+            Console.Write("1 1 ");
+            for(int i=0; i<count-2; i++)
+            {
+                int temp = prev1 + prev2;
+                Console.Write(temp + " ");
+                prev1 = prev2;
+                prev2 = temp;
+            }
+        }
+    }
+}
---
+++ FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fib = new Fibonacci();
+            Console.WriteLine("get length of fibonacci sequence");
+            var length = int.Parse(Console.ReadLine());
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("Recorsive:");
+            sw.Start();
+            fib.fibonacci_Rec(length);
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Duration of calculation series: " + sw.ElapsedMilliseconds+"ms");
+            Console.WriteLine("Iterative:");
+            sw.Restart();
+            fib.fibonacciIt(length);
+            sw.Stop();
+            Console.WriteLine();
+            Console.WriteLine("Duration of calculation series: " + sw.ElapsedMilliseconds + "ms");
+            Console.ReadKey();
+        }
+    }
+}
